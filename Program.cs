ea84945c6b240ca8a537470537e8b4dd069aa8b1
Prateek <prateek.9n@gmail.com>
2024-01-27 19:35:52 -0500

    writing nested switch case to build main menu options.

--- Program.cs
+++ Program.cs
@@ -21,14 +21,46 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
                {
                    switch(pet)
                    {
-                    case 1: Console.WriteLine("You have choosen Cat");
+                    case 1:
+                        Console.WriteLine("your choice is {0}", pet);
+                        Console.WriteLine("You have choosen Cat,What would you like to name your pet?");
+                        String petname= Console.ReadLine();
                         
+                        Console.WriteLine("Welcome, {0} Let's take a good care of it", petname);
+
+                        Console.WriteLine("Main Menu:");
+                        Console.WriteLine("1. Feed \n2. Play with \n3. Let rest \n4. Check status \n5. Exit ");
+                        bool mm1 = int.TryParse(Console.ReadLine(), out int mm);
+                        if(mm1==true)
+                        { 
+                        switch (mm)
+                            { case 1:
+                            }
+
+
                         break;
                         
-                    case 2: Console.WriteLine("You have choosen Dog");
+                    case 2:
+                        Console.WriteLine("your choice is {0}", pet);
+                        Console.WriteLine("You have choosen Dog,What would you like to name your pet?");
+                        String petname1 = Console.ReadLine();
+
+                        Console.WriteLine("Welcome, {0} Let's take a good care of it", petname1);
+
+                        Console.WriteLine("Main Menu:");
                         break;
                         
-                    case 3: Console.WriteLine("You have choosen Rabbit");
+                    case 3:
+                        Console.WriteLine("your choice is {0}", pet);
+                        Console.WriteLine("You have choosen Rabbit,What would you like to name your pet?");
+                        String petname2 = Console.ReadLine();
+
+                        Console.WriteLine("Welcome, {0} Let's take a good care of it", petname2);
+
+                        Console.WriteLine("Main Menu:");
+                        break;
+                    default:
+                        Console.WriteLine("You have to choose from only these three");
                         break;
                    }
                }
