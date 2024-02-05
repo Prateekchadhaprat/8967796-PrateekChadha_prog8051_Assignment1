e99be76c294bd95f7297f76f628113a7bd10c89b
Prateek <prateek.9n@gmail.com>
2024-02-05 13:58:53 -0500

    Added comments in switch cases and in other areas of the code for better readability. Also, removed the duplication of code to make it more efficient.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            // Displaying  options for pet types
+
             Console.WriteLine("Please choose a type of Pet");
             Console.WriteLine("1. Cat \n2. Dog \n3. Rabbit");
+
+            // Using bool validation to ensure correct type of pet by user
             bool pettypecheck = int.TryParse(Console.ReadLine(), out int pettype); // using bool validation to ensure correct type of pet by user
 
             int hunger = 5; // these are pet attributes
@@ -16,7 +20,7 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
 
             string petName = string.Empty;
 
-            switch (pettype)
+            switch (pettype) // Using switch case to handle user input for pet type selection
             {
                 case 1:
                     Console.WriteLine("your choice is {0}", pettype);
@@ -37,21 +41,22 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
 
             Console.WriteLine("Welcome, {0} Let's take a good care of it", petName);
 
-            while (true)
+            while (true) // Implemented main loop for pet care actions.
             {
-                Console.WriteLine("Main Menu:"); // main menue for pet care actions
+                Console.WriteLine("Main Menu:"); // main menu for pet care actions
                 Console.WriteLine("1. Feed \n2. Play with \n3. Let rest \n4. Check status \n5. Exit ");
 
                 bool actioncheck = int.TryParse(Console.ReadLine(), out int action); //user input for action selections
 
                 if (actioncheck)
                 {
-
-                    switch (action)
+                    // for handling user selected actions implemnted another switch in if condition
+                    switch (action) 
                     {
-                        case 1:
+                        case 1: // Feeding the pet
 
-                            if (hunger - 1 < 1)
+
+                            if (hunger - 1 < 1) //// Adjusting hunger and health attributes
                             {
                                 hunger = 1;
                             }
@@ -70,9 +75,9 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
                             Console.WriteLine($"You fed {petName} His hunger is decreased, and health improves slightly ");
                             break;
 
-                        case 2:
+                        case 2: // Playing with pet
 
-                            if (happiness - 1 < 1) // to ensure health doesn't go below 1 
+                            if (happiness - 1 < 1)  // Adjusting happiness and hunger attributes
                             {
                                 happiness = 1;
                             }
@@ -93,8 +98,10 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
                             Console.WriteLine($"You played with{petName} , his happiness increases and hunger slightly increases");
                             break;
 
-                        case 3:
-                            if (health + 1 > 10)// to ensure health doesn't exceed 10
+                        case 3: // Letting the pet rest
+
+
+                            if (health + 1 > 10) // Adjusting health and happiness attributes
                             {
                                 health = 10;
                             }
@@ -112,11 +119,15 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
                             }
                             Console.WriteLine($"Your {petName} is resting. his health is improving and happniness is decreasing slightly\".");
                             break;
-                        case 4:
+
+                        case 4: // Checking and displaying pet status
+
                             Console.WriteLine($"\n{petName}status");
                             Console.WriteLine($"Hunger:- {hunger}/10");
                             Console.WriteLine($"Happiness:- {happiness}/10");
                             Console.WriteLine($"Health:-{health}/10");
+
+                            // Displaying warnings for critical statuses
 
                             if (hunger >= 8)
                                 Console.WriteLine("Warning : Hunger status is critically high for your pet");
@@ -125,11 +136,11 @@ namespace _8967796_PrateekChadha_prog8051_Assignment1
                             if (health <= 2)
                                 Console.WriteLine("Warning: Health status is critically low for your pet");
                             break;
-                        case 5:
+                        case 5: // // Exiting from the game
                             Console.WriteLine($"Thank you, exiting the simulator. Bye,{petName}");
-                            //Environment.Exit(0);
+                            
                             return;
-                        // break;
+                       
                         default:
                             Console.WriteLine("Invalid choice, please choose option from 1 to 5 only");
                             break;
